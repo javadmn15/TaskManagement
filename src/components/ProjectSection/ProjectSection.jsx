eae3c3d85124648bcb4768581f54f4806f1b224a
javadjamali256 <javadjamali256@gmail.com>
2024-06-26 14:07:47 +0330

    project section added

---
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../ProjectSection/ProjectSection.css";
+
+function ProjectSection() {
+  return (
+    <div className='projects-container'>
+        <h1>All Projects</h1>
+        <span>Managing all your tasks at ease with task manager</span>
+    </div>
+  )
+}
+
+export default ProjectSection
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TopNavBar from '../../components/TopNavBar/TopNavBar'
 import InfoSection from '../../components/InfoSection/InfoSection'
+import ProjectSection from '../../components/ProjectSection/ProjectSection'
 
 function Home() {
   return (
     <div>
         <TopNavBar/>
         <InfoSection/>
+        <ProjectSection/>
     </div>
   )
 }
